6b97432780fe86494b8eaf95b4ba5844e4d33e22
wcordelo <will96@mit.edu>
2018-05-16 22:20:13 -0400

    fuzzy vault

--- projects/KeyChain/fuzzy_vault.go
+++ projects/KeyChain/fuzzy_vault.go
@@ -13,9 +13,12 @@ var (
 	degree int     = 4
 	t      float64 = 10
 	r      float64 = 40
+	digits int     = 2
 )
 
-func GetCoefficients(word string) []float64 {
+// Encode a string into coeeficients. The highest dimension is the last element
+// in the array.
+func getCoefficients(word string) []float64 {
 	word = strings.ToUpper(word)
 	n := len(word) / degree
 	if n < 1 {
@@ -25,7 +28,7 @@ func GetCoefficients(word string) []float64 {
 
 	for i := 0; i < len(word); i += n {
 
-		top := int(math.Min(float64(len(word)), float64(i + n)))
+		top := int(math.Min(float64(len(word)), float64(i+n)))
 		substring = append(substring, word[i:top])
 	}
 
@@ -34,14 +37,15 @@ func GetCoefficients(word string) []float64 {
 	for _, sub := range substring {
 		num := 0.0
 		for i, char := range sub {
-			num += float64(int(char)) * math.Pow10(2*i)
+			num += float64(int(char)) * math.Pow10(digits*i)
 		}
 		coeffs = append(coeffs, num)
 	}
 	return coeffs
 }
 
-func EvalAt(x float64, coeffs []float64) float64 {
+// Evaluate a polynomial at a point
+func evalAt(x float64, coeffs []float64) float64 {
 	ret := 0.0
 
 	for i, coefficient := range coeffs {
@@ -50,29 +54,98 @@ func EvalAt(x float64, coeffs []float64) float64 {
 	return ret
 }
 
+func approxEqual(a, b, epsilon float64) bool {
+	return math.Abs(a-b) < epsilon
+}
+
+// Return a regressed polynomial from a set of points
+func polyfit(X, Y []float64) []float64 {
+	ret := make([]float64, degree+1)
+
+	a := vandermonde(X, degree)
+	b := mat64.NewDense(len(Y), 1, Y)
+	c := mat64.NewDense(degree+1, 1, nil)
+
+	qr := new(mat64.QR)
+	qr.Factorize(a)
+
+	err := c.SolveQR(qr, false, b)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	} else {
+		mat64.Col(ret, 0, c)
+
+		for i := 0; i < len(ret); i++ {
+			ret[i] = round(ret[i], 0.01)
+		}
+
+		return ret
+	}
+}
+
+func vandermonde(a []float64, degree int) *mat64.Dense {
+	x := mat64.NewDense(len(a), degree+1, nil)
+	for i := range a {
+		for j, p := 0, 1.; j <= degree; j, p = j+1, p*a[i] {
+			x.Set(i, j, p)
+		}
+	}
+	return x
+}
+
+func round(x, unit float64) float64 {
+	return math.Round(x/unit) * unit
+}
+
+func maxFloat64Slice(s []float64) float64 {
+	m := math.Inf(-1)
+	for _, e := range s {
+		if e > m {
+			m = e
+		}
+	}
+	return m
+}
+
+func generateRandomTemplate(n int) []float64 {
+	var ret []float64
+	for i := 0; i < n; i++ {
+		y := rand.Float64() * math.Pow10(digits)
+		ret = append(ret, y)
+	}
+	return ret
+
+}
+
+// Lock a string in a fuzzy fault given a fingerprint template
 func Lock(secret string, template []float64) [][]float64 {
 	var vault [][]float64
-	coeffs := GetCoefficients(secret)
+	coeffs := getCoefficients(secret)
 	fmt.Printf("Coded Coeffs: %v\n", coeffs)
 
 	maxY := math.Inf(-1)
 
 	for _, point := range template {
-		y := EvalAt(point, coeffs)
+		y := evalAt(point, coeffs)
 		if y > maxY {
 			maxY = y
 		}
 		vault = append(vault, []float64{point, y})
 	}
 
-	maxX := MaxFloat64Slice(template)
+	maxX := maxFloat64Slice(template)
 
+	// Add random floats to disguise points
 	for i := t; i < r; i++ {
+		// Increase the max to reveal less information about the vault
 		xI := rand.Float64() * maxX * 1.1
 		yI := rand.Float64() * maxY * 1.1
 		vault = append(vault, []float64{xI, yI})
 	}
 
+	// Shuffle the order of the vault
 	rand.Shuffle(len(vault), func(i, j int) {
 		vault[i], vault[j] = vault[j], vault[i]
 	})
@@ -80,14 +153,10 @@ func Lock(secret string, template []float64) [][]float64 {
 	return vault
 }
 
-func ApproxEqual(a, b, epsilon float64) bool {
-	return math.Abs(a-b) < epsilon
-}
-
 func Unlock(template []float64, vault [][]float64) []float64 {
 	project := func(x float64) (float64, float64) {
 		for _, point := range vault {
-			if ApproxEqual(x, point[0], 0.001) {
+			if approxEqual(x, point[0], 0.001) {
 				return x, point[1]
 			}
 		}
@@ -107,78 +176,34 @@ func Unlock(template []float64, vault [][]float64) []float64 {
 	return polyfit(tempX, tempY)
 }
 
-func polyfit(X, Y []float64) []float64 {
-	ret := make([]float64, degree + 1)
-
-	a := Vandermonde(X, degree)
-	b := mat64.NewDense(len(Y), 1, Y)
-	c := mat64.NewDense(degree+1, 1, nil)
-
-	qr := new(mat64.QR)
-	qr.Factorize(a)
-
-	err := c.SolveQR(qr, false, b)
-
-	// fmt.Println(c.Dims())
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	} else {
-		mat64.Col(ret, 0, c)
-
-		for i := 0; i < len(ret); i++ {
-			ret[i] = round(ret[i], 0.01)
-		}
-
-		return ret
-	}
-}
-
-func Vandermonde(a []float64, degree int) *mat64.Dense {
-	x := mat64.NewDense(len(a), degree+1, nil)
-	for i := range a {
-		for j, p := 0, 1.; j <= degree; j, p = j+1, p*a[i] {
-			x.Set(i, j, p)
-		}
-	}
-	return x
-}
-
-func round(x, unit float64) float64 {
-	return math.Round(x/unit) * unit
-}
-
 func Decode(coeffs []float64) string {
 	s := ""
 	for _, c := range coeffs {
 
-		if c == 0 {continue}
+		if c == 0 {
+			continue
+		}
 
 		log100 := (int(math.Log10(c)) + 1) / 2
 
 		for i := 0; i < log100; i++ {
-			char := int(c / math.Pow(100, float64(i))) % 100
+			char := int(c/math.Pow10(digits*i)) % int(math.Pow10(digits))
 			s += string(rune(char))
 		}
 	}
 	return s
 }
 
-func MaxFloat64Slice(s []float64) float64 {
-	m := math.Inf(-1)
-	for _, e := range s {
-		if e > m {
-			m = e
-		}
-	}
-	return m
-}
+func main() {
+	rand.Seed(42)
+	fmt.Printf("Locking Phrase in Fuzzy Vault\n")
 
-func generateRandomTemplate(n int) []float64 {
-	var ret []float64
-	for i := 0; i < n; i++ {
-		y := rand.Float64() * 100
-		ret = append(ret, y)
-	}
-	return ret
+	word := "WHAT'S GOOD!"
+	template := generateRandomTemplate(30)
+	vault := Lock(word, template)
+	fmt.Println("vault:", vault)
+	coeffs := Unlock(template, vault)
+	fmt.Println("Decoded Coeffs:", coeffs)
+	ret := Decode(coeffs)
+	fmt.Println("Decoded String:", ret)
 }
